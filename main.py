0636684495eb9faf257e0fe443865222a6ce0362
Amelia Fitzsimmons <113189695+AmeliaFitzsimmons@users.noreply.github.com>
2022-11-25 14:55:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from math import sqrt
+a = float(input('Please enter side "a" of your right triangle:'))
+b = float(input('Please enter side "b" of your right triangle:'))
+c = sqrt(a**2 + b**2)
+print("length of c:", c)
